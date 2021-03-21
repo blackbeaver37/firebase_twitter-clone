1e008b98af878666ca5bc354192dd619c4f8eb78
YoonSoo <fhkaldh@naver.com>
2021-03-22 01:45:10 +0900

    21.03.22

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,3 +1,18 @@
 import React from 'react';
+import { authService } from 'myFirebase';
+import { useHistory } from 'react-router';
 
-export default () => <span>Profile</span>;
+const Profile = () => {
+    const history = useHistory();
+    const onLogOutClick = () => {
+        authService.signOut();
+        history.push("/");
+    }
+    return (
+        <>
+            <button onClick={onLogOutClick}>Log Out</button>
+        </>
+    )
+}
+
+export default Profile;
